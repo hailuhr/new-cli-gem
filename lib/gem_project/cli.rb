8085ec21ab44314abdd262d20a2dcc2e978ad066
Flatiron School <github email address>
2016-08-05 11:38:47 -0400

    branch test

--- lib/gem_project/cli.rb
+++ lib/gem_project/cli.rb
@@ -2,7 +2,9 @@
 class GemProject::CLI
 
   def call
-    puts "Welcome to Community Meetings! Learn more about what's happening in your neighborhood. Select your neighborhood of choice to know what's being discussed for the neighborhood and the details for its community meetings.\n"
+    puts "Welcome to Community Meetings! Learn more about what's happening in your neighborhood. Enter the number for one of your neighborhoods below to find out about the community meetings and what's being discussed for it!\n"
+    # puts "\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. Type exit to quit the program.\n"
+    puts
     menu
     goodbye
   end
@@ -10,30 +12,35 @@ class GemProject::CLI
   def list_
     @meetings = GemProject::CommunityMeeting.scrape_all
     count = 0
+    puts
     @meetings.each do |meeting|
+
         puts "#{count += 1}. #{meeting.neighborhoods.join(",")}"
       end
   end
 
     def menu
-      puts "\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. To quit the program type exit.\n"
+      # puts "\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. To quit the program type exit.\n"
       list_
       puts
       # binding.pry
       input = nil
 
+      # while input != "exit"
       while input != "exit"
+      # binding.pry
+      # puts "\n\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. To quit the program type exit."
       input = gets.strip.downcase
           if input.to_i > 0
             print_meeting_info(input)
-            menu
+            # menu
+            puts "\n\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. To exit to quit the program."
           elsif input == "list"
             list_
             puts "\n\nEnter the number of the neighborhood you would like to get more information on. Type list to see all options again. To quit the program type exit."
-          else
-            puts "Please type list, exit, or an option number.\n\n"
           end
-      end
+          
+        end
 
     end
 
@@ -46,28 +53,32 @@ class GemProject::CLI
       puts "\nWhat information would you like about the community's meeting?\n"
       puts "\nEnter 'time' for the meeting dates and times, 'address' for the meeting's address, 'phone' for the board's phone number, 'agenda' for the latest agenda, 'website' for the webpage, or 'menu' for the main page \n"
 
-      while i != "menu"
+      until i == "menu"
+      # puts "\nType 'menu' for the main page, 'website' for the webpage, 'time' for the meeting dates and hours, 'address' for the meeting's address, 'phone' for the board's phone number, or 'agenda' for the latest agenda. \n"
       i = gets.strip.downcase
+
           if i == "address"
+            puts
             puts meeting.address
             puts "\nEnter time, address, phone, agenda, website, or menu.\n"
           elsif i == "website"
+            puts
             puts meeting.url
             puts "\nEnter time, address, phone, agenda, website, or menu.\n"
           elsif i == "time"
+            puts
             puts meeting.hours
             puts "\nEnter time, address, phone, agenda, website, or menu.\n"
           elsif i == "phone"
+            puts
             puts meeting.phone
             puts "\nEnter time, address, phone, agenda, website, or menu.\n"
             #last agenda
             #open the website
           elsif i == "agenda"
+            puts
             puts meeting.agenda
             puts "\nEnter time, address, phone, agenda, website, or menu.\n"
-          else
-            #why does the below repeat when main is typed
-            puts "\nType 'menu' for the main page, 'website' for the webpage, 'time' for the meeting dates and hours, 'address' for the meeting's address, 'phone' for the board's phone number, or 'agenda' for the latest agenda. \n"
           end
       end
     end
